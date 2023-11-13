9dab7faef102a387cfb6558df121af01c104c413
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-11-13 20:16:20 +0700

    [CREATE] logic to handle get detail artwork

--- src/controllers/gallery.controller.js
+++ src/controllers/gallery.controller.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const ResponseTemplate = require('../helper/response.helper');
+const { param } = require('../routes/v1/route');
 const prisma = new PrismaClient();
 
 async function GetAll(req, res) {
@@ -21,4 +22,30 @@ async function GetAll(req, res) {
   }
 }
 
-module.exports = { GetAll };
+async function GetById(req, res) {
+  const { id } = req.params;
+
+  try {
+    const artwork = await prisma.artwork.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!artwork) {
+      return res
+        .status(404)
+        .json(
+          ResponseTemplate(artwork, 'the artwork was not found', null, 404)
+        );
+    }
+
+    res.status(200).json(ResponseTemplate(artwork, 'success', null, 200));
+  } catch (error) {
+    return res
+      .status(500)
+      .json(ResponseTemplate(null, 'internal server error', error, 500));
+  }
+}
+
+module.exports = { GetAll, GetById };
--- src/routes/v1/route.js
+++ src/routes/v1/route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const storage = require('../../lib/multer');
 const Upload = require('../../controllers/upload.controller');
-const { GetAll } = require('../../controllers/gallery.controller');
+const { GetAll, GetById } = require('../../controllers/gallery.controller');
 
 router.post('/v1/upload', storage.Image.single('images'), Upload);
 router.get('/v1/artworks', GetAll);
+router.get('/v1/artworks/:id', GetById);
 
 module.exports = router;
