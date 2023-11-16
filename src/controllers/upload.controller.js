08f851435664fa06b200e2c5de625cc92af2eff9
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-11-16 19:35:13 +0700

    [ADD] logic to handle media upload to cloud storage

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,19 +1,22 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const ResponseTemplate = require('../helper/response.helper');
+const imagekit = require('../lib/imagekit');
 
 async function Upload(req, res) {
   try {
     const { title, description } = JSON.parse(req.body.data);
-    const imageUrl = `${req.protocol}://${req.get('host')}/images/${
-      req.file.filename
-    }`;
+
+    const uploadFile = await imagekit.upload({
+      fileName: req.file.originalname,
+      file: stringFile,
+    });
 
     const newImage = await prisma.artwork.create({
       data: {
         title: title,
         description: description,
-        imageUrl: imageUrl,
+        imageUrl: uploadFile.url,
       },
     });
 
